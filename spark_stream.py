e0521f3258fa53b2bdd09713966edcbe7c506d0c
LeenKharboutli <leen.kharboutli@protonmail.com>
2024-11-11 13:42:31 -0500

    modified spark_stream to address all errors.

--- spark_stream.py
+++ spark_stream.py
@@ -4,8 +4,20 @@ from datetime import datetime
 from cassandra.auth import PlainTextAuthProvider
 from cassandra.cluster import Cluster
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, col
+from pyspark.sql.functions import from_json, col, expr
 from pyspark.sql.types import *
+
+# from pyspark.sql import SparkSession
+
+# spark = SparkSession.builder.getOrCreate()
+# scala_version = spark.version
+# print(f"Scala version used by Spark: {scala_version}")
+
+
+
+# you need java runtime installed
+# https://www.java.com/en/download/
+
 
 # Create keyspace on cassandra (keyspace is like a schema)
 # multiple tables per keyspace
@@ -20,23 +32,23 @@ def create_keyspace(session):
 
 
 def create_table(session):
-    """
-    
-    """
     session.execute("""
-        CREATE TABLE IF NOT EXISTS spark_streams.created_users (
-            id UUID PRIMARY KEY,
-            first_name TEXT,
-            last_name TEXT,
-            gender TEXT
-            address TEXT,
-            post_code TEXT,
-            email TEXT,
-            username TEXT,
-            registered_date TEXT,
-            phone TEXT,
-            picture TEXT);
+    CREATE TABLE IF NOT EXISTS spark_streams.created_users (
+        id UUID PRIMARY KEY,
+        first_name TEXT,
+        last_name TEXT,
+        gender TEXT,
+        address TEXT,
+        postcode TEXT,
+        dob TEXT,
+        email TEXT,
+        username TEXT,
+        registered_date TEXT,
+        phone TEXT,
+        picture TEXT);
     """)
+
+    print("Table created successfully!")
 
 def insert_data(session, **kwargs):
     """
@@ -78,13 +90,26 @@ def create_spark_connection():
     """
     s_conn = None
 
+                    # "com.datastax.spark:spark-cassandra-connector_2.12:3.5.1,"
+                    #                        "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.3") \
+# org.apache.commons:commons-pool2:2.8.0,
     try:
+        logging.error("Beginning s_con setup...")
         s_conn = SparkSession.builder\
             .appName('SparkDataStreaming')\
-            .config('spark.jars.packages', "com.datastax.spark:spark-cassandra-connector_2.13:3.51",
-                    "org.apache.spark:spark-sql-kafka-0-10_2.13:3.4.1") \
+            .config('spark.jars.packages', "com.datastax.spark:spark-cassandra-connector_2.12:3.5.1,"
+                                    "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.3,"
+                                    "org.apache.kafka:kafka-clients:3.4.1,"
+                                    "com.datastax.spark:spark-cassandra-connector-assembly_2.12:3.4.1,"
+                                    "org.apache.cassandra:java-driver-core:4.18.1,"
+                                    "com.typesafe:config:1.4.2,"
+                                    "org.apache.spark:spark-streaming-kafka-0-10_2.12:3.5.3,"
+                                    "org.apache.kafka:kafka_2.12:3.4.1,"
+                                    "org.apache.commons:commons-pool2:2.11.1,"
+                                    "org.apache.spark:spark-token-provider-kafka-0-10_2.12:3.5.3") \
             .config('spark.cassandra.connection.host', 'localhost') \
             .getOrCreate()
+        logging.error("Completed s_con setup...")
         s_conn.sparkContext.setLogLevel("ERROR")
         logging.info("Spark connection created successfully!")
     except Exception as e:
@@ -109,9 +134,9 @@ def connect_to_kafka(spark_conn):
                     .format('kafka') \
                     .option('kafka.bootstrap.servers', 'localhost:9092') \
                     .option('subscribe', 'users_created') \
-                    .option('startingOffsets, earliest') \
-                    .load()
-        logging.info("kafka dataframe created successfully")
+                    .option('startingOffsets', 'earliest') \
+                    .option('failOnDataLoss', 'false').load()
+        logging.error("kafka dataframe created successfully")
     except Exception as e:
         logging.warning(f"kafka dataframe could not be created:\n{e}")
 
@@ -158,11 +183,17 @@ def create_selection_df_from_kafka(spark_df):
         StructField("picture", StringType(), False)
     ])
 
-    sel = spark_df.selectExpr("CAST(value AS STRING)")\
-        .select(from_json(col('value'), schema).alias('data.*'))
-    logging.info(sel)
+    # sel = spark_df.selectExpr("CAST(value AS STRING)")\
+    #     .select(from_json(col('value'), schema).alias('data.*'))
+    # logging.info(sel)
+    # return sel
 
-    return sel
+    return spark_df.selectExpr("CAST(value AS STRING)") \
+        .select(from_json(col('value'), schema).alias('data')) \
+        .select('data.*') \
+        .withColumn('id', expr('uuid()'))  # Convert string ID to UUID
+
+
 
 
 if __name__ == "__main__":
@@ -175,7 +206,9 @@ if __name__ == "__main__":
         selection_df = create_selection_df_from_kafka(spark_df)
         session = create_cassandra_connection()
 
+        
         if session is not None:
+            # Finally getting here! Downloading kafka-clients to /venv/lib/python3.9/site-packages/pyspark/jars/ did it
             create_keyspace(session)
             create_table(session)
             # insert_data(session)
